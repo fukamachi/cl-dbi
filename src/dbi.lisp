1cacb587f96d0f01ce4425467015b9f06e32e580
Eitaro Fukamachi <e.arrows@gmail.com>
2018-12-07 17:23:22 +0900

    Raise a condition in commit/rollback not to run them twice at the last of 'with-transaction'.

--- src/dbi.lisp
+++ src/dbi.lisp
@@ -23,7 +23,8 @@
                 :release-savepoint
                 :*current-savepoint*
                 :ping
-                :row-count)
+                :row-count
+                :transaction-done-condition)
   (:import-from :bordeaux-threads
                 :current-thread
                 :thread-alive-p)
@@ -151,34 +152,44 @@
 
 @export
 (defmacro with-savepoint (conn &body body)
-  (let ((ok (gensym "SAVEPOINT-OK"))
+  (let ((done (gensym "SAVEPOINT-DONE"))
+        (ok (gensym "SAVEPOINT-OK"))
         (conn-var (gensym "CONN-VAR")))
-    `(let* (,ok
+    `(let* (,done
+            ,ok
             (,conn-var ,conn)
             (*current-savepoint* (generate-random-savepoint)))
        (savepoint ,conn-var *current-savepoint*)
-       (unwind-protect (multiple-value-prog1
-                           (progn ,@body)
-                         (setf ,ok t))
-         (if ,ok
-             (release-savepoint ,conn-var)
-             (rollback-savepoint ,conn-var))))))
+       (unwind-protect
+            (handler-case (multiple-value-prog1
+                              (progn ,@body)
+                            (setf ,ok t))
+              (transaction-done-condition () (setf ,done t)))
+         (unless ,done
+           (if ,ok
+               (release-savepoint ,conn-var)
+               (rollback-savepoint ,conn-var)))))))
 
 (defvar *in-transaction* nil)
 
 (defmacro %with-transaction (conn &body body)
-  (let ((ok (gensym "TRANSACTION-OK"))
+  (let ((done (gensym "TRANSACTION-DONE"))
+        (ok (gensym "TRANSACTION-OK"))
         (conn-var (gensym "CONN-VAR")))
-    `(let* (,ok
+    `(let* (,done
+            ,ok
             (,conn-var ,conn)
             (*in-transaction* (cons ,conn-var *in-transaction*)))
        (begin-transaction ,conn-var)
-       (unwind-protect (multiple-value-prog1
-                           (progn ,@body)
-                         (setf ,ok t))
-         (if ,ok
-             (commit ,conn-var)
-             (rollback ,conn-var))))))
+       (unwind-protect
+            (handler-case (multiple-value-prog1
+                              (progn ,@body)
+                            (setf ,ok t))
+              (transaction-done-condition () (setf ,done t)))
+         (unless ,done
+           (if ,ok
+               (commit ,conn-var)
+               (rollback ,conn-var)))))))
 
 @export
 (defmacro with-transaction (conn &body body)
