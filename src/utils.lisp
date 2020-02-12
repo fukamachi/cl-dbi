c1157dbf7b876cff78f43c0efd4593548edab56e
Eitaro Fukamachi <e.arrows@gmail.com>
2020-02-12 10:57:46 +0900

    Define alias types.

--- src/utils.lisp
+++ src/utils.lisp
@@ -7,11 +7,13 @@
 (defmacro defclass/a (name superclasses slots &rest class-options)
   `(progn
      (defclass ,name ,superclasses ,slots ,@class-options)
+     (deftype ,(intern (format nil "<~A>" (symbol-name name))) () ',name)
      (setf (find-class ',(intern (format nil "<~A>" (symbol-name name))))
            (find-class ',name))))
 
 (defmacro define-condition/a (name parent-types slots &body options)
   `(progn
      (define-condition ,name ,parent-types ,slots ,@options)
+     (deftype ,(intern (format nil "<~A>" (symbol-name name))) () ',name)
      (setf (find-class ',(intern (format nil "<~A>" (symbol-name name))))
            (find-class ',name))))
